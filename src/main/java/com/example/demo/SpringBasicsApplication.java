b52351a97fe9bb22c049950580078f612f77a946
GANAGEN <nikhil.ganage@theaa.com>
2019-10-11 15:33:51 +0530

    Create project structure

---
+++ src/main/java/com/example/demo/SpringBasicsApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasicsApplication.class, args);
+		
+		System.out.println("Hello World!!");
+		System.out.println("My first Spring Boot program!!");
+	}
+
+}
